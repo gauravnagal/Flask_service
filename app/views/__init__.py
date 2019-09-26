482116471fbbb591572f2be9bfa9fa3182c10b84
gauravnagal <47132774+gauravnagal@users.noreply.github.com>
2019-09-26 01:02:34 -0400

    Update __init__.py

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,6 +1,7 @@
 from app import flask_app as app
 import json
 from datetime import datetime
+import numpy as np
 
 
 @app.route("/heartbeat")
@@ -13,6 +14,31 @@ def heartbeat():
         }
     )
 
+@app.route("/sum", methods = ['POST'])
+def sum():
+    data =  request.json
+    x = int(data['x'])
+    y = int(data['y'])
+    
+    result = {x + y}
+    return json.dumps({'Sum': result})
+
+@app.route("/minimum", methods = ['POST'])
+def minimum():
+    data =  request.json
+    values = data['values']
+    result = min(values)
+    return json.dumps({'Minimum': result})
+
+@app.route("/product", methods = ['POST'])
+def product():
+    data =  request.json
+    values = data['values']
+    if len(values):
+        result = int(np.prod(values))
+    else :
+        result = 0
+    return json.dumps({'Product': result})
 
 @app.before_first_request
 def load_app():
